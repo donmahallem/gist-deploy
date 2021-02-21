076a90342dfb7eb2e00f43df7f92f6f08cb06e6c
DonMahallem <donmahallem@users.noreply.github.com>
2021-02-21 18:45:34 +0100

    chore(deps): add ts-node

--- src/action.ts
+++ src/action.ts
@@ -1,13 +1,15 @@
 import * as core from "@actions/core";
+import * as github from '@actions/github'
+import { syncFiles } from '@donmahallem/sync-gist';
 import { getConfig, IConfig } from "./inputs";
 import { logConfig } from "./log-config";
-import { syncFiles } from "./sync-files";
 
 export const action = async () => {
     const config: IConfig = await getConfig();
+    const octokit = github.getOctokit(config.github_secret)
     logConfig(config);
     if (config.dry_run !== true) {
-        syncFiles(config, core);
+        syncFiles(config, octokit);
     } else {
         core.info(`Not syncing files. Dry run!`);
     }
--- src/inputs/parse-config-file.ts
+++ src/inputs/parse-config-file.ts
@@ -1,9 +1,9 @@
 import { IConfigFile } from './types';
-import { readFile } from 'fs/promises';
+import { promises as fsp } from 'fs';
 import { validateConfig } from './validate-config';
 
 export const parseConfigFile: (filePath: string) => Promise<IConfigFile> = (filePath: string): Promise<IConfigFile> => {
-    return readFile(filePath)
+    return fsp.readFile(filePath)
         .then((fileContent: Buffer): IConfigFile => {
             const parsedJson: IConfigFile = JSON.parse(fileContent.toString('utf8'));
             validateConfig(parsedJson);
